bd9c3629633f1950082197787255e8a3ce0a3713
Jason Karns <jason@karns.name>
2016-05-17 09:30:25 -0400

    Refactor the BRE vars

--- semver.sh
+++ semver.sh
@@ -10,11 +10,15 @@ RE_MET="$_met_part(\.$_met_part)*"
 RE_VER="[ \t]*$RE_NUM(-$RE_LAB)?(\+$RE_MET)?"
 
 BRE_DIGIT='[0-9]\{1,\}'
-BRE_ALPHA='[0-9a-zA-Z-]\{1,\}'
-BRE_NUM="$BRE_DIGIT\(\.$BRE_DIGIT\)\{0,2\}"
-BRE_PRE="$BRE_ALPHA\(\.$BRE_ALPHA\)*"
-BRE_MET="$BRE_ALPHA"
-BRE_VER="$BRE_NUM\(-$BRE_PRE\)\{0,1\}\(+$BRE_MET\)\{0,1\}"
+BRE_ALNUM='[0-9a-zA-Z-]\{1,\}'
+BRE_IDENT="$BRE_ALNUM\(\.$BRE_ALNUM\)*"
+
+BRE_MAJOR="$BRE_DIGIT"
+BRE_MINOR="\(\.$BRE_DIGIT\)\{0,1\}"
+BRE_PATCH="\(\.$BRE_DIGIT\)\{0,1\}"
+BRE_PRERE="\(-$BRE_IDENT\)\{0,1\}"
+BRE_BUILD="\(+$BRE_ALNUM\)\{0,1\}"
+BRE_VERSION="${BRE_MAJOR}${BRE_MINOR}${BRE_PATCH}${BRE_PRERE}${BRE_BUILD}"
 
 filter()
 {
@@ -297,8 +301,8 @@ resolve_rule()
 {
     local rule operator operands
     rule="$1"
-    operator="$( echo "$rule" | sed "s/$BRE_VER/#/g" )"
-    operands=( $( echo "$rule" | grep -o "$BRE_VER") )
+    operator="$( echo "$rule" | sed "s/$BRE_VERSION/#/g" )"
+    operands=( $( echo "$rule" | grep -o "$BRE_VERSION") )
 
     case "$operator" in
         '*')     echo "all" ;;
