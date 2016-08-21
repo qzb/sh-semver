qzb/sh-semver

commit fff3821c052aac9e20dd8ee9217df3b3daf7e5ac

SC2059: Don't use variables in the printf format string. Use printf "..%s.." "$foo".

semver.sh
@@ -514,5 +514,5 @@ EOF
 done
 
 if [ -n "$output" ]; then
-    printf "$output"
+    echo "$output"
 fi

commit dacea514865415f53417db2fd844746f3681d205

Ensure "\n" chars are printed as newlines
The switch from printf to echo was incorrect (introduced by
fff3821c052aac9e20dd8ee9217df3b3daf7e5ac).

Rather, `printf` should be used (but not as it was originally).
Variables themselves ought not be used in the format string. And to
portably print newlines and other escape sequences, the '%b' format
specifier should be used. This properly mimics echo behavior across
platforms.

http://pubs.opengroup.org/onlinepubs/9699919799/utilities/echo.html#tag_20_37_16

semver.sh
@@ -505,5 +505,5 @@ EOF
 done
 
 if [ -n "$output" ]; then
-    echo "$output"
+    printf '%b' "$output"
 fi